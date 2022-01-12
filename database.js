f1fa658f333f9e5ae85e43815f2afc2ad808e6ac
AJOsmaston <dajoxx@gmail.com>
2022-01-12 17:31:30 +0000

    added mongoose and connection to mongoDB

---
+++ database.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose');
+
+class Database {
+  constructor() {
+    this._connect()
+  }
+  
+  _connect() {
+    mongoose.connect(process.env.DB, { useNewUrlParser: true })
+      .then(() => console.log('Database connected successfully'))
+      .catch(err => {console.error('Cannot connect to the database')
+    })
+  }
+}
+
+module.exports = new Database()

b86267487cd9635bd67d45e4a89044ca11b44698
AJOsmaston <dajoxx@gmail.com>
2022-01-12 18:19:23 +0000

    post route for adding a new feedback item

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ require('dotenv').config()
 require('./database');
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const Feedback = require('./models/feedback')
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+
+app.get('/feedback', (req, res) => {
   const displayFeedback = async () => {
     const feedback = await Feedback.find().sort({ id: -1 }).limit(10)
     res.status(200).json(feedback)
   }
   displayFeedback();
+})
+
+app.post('/feedback', (req, res) => {
+  console.log(req.body)
+  const addFeedback = async () => {
+    const newFeedback = new Feedback({ id: req.body.id, text: req.body.text, rating: req.body.rating })
+    await newFeedback.save()
+    console.log(`saved ${newFeedback}`)
+  };
+  addFeedback();
 })
 
 app.listen(port, () => {
--- models/feedback.js
+++ models/feedback.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FeedbackSchema = new Schema({
-  id: Number,
+  id: String,
   text: String,
   rating: Number,
 })

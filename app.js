7567e0ba4c5142854150153f323076bd5414ebcf
AJOsmaston <dajoxx@gmail.com>
2022-01-13 12:07:53 +0000

    error catching and logging added for debugging

--- app.js
+++ app.js
@@ -9,43 +9,48 @@ const Feedback = require('./models/feedback')
 
 app.use(bodyParser.json());
 
-app.get('/feedback', (req, res) => {
+app.get('/feedback', (req, res, next) => {
   const displayFeedback = async () => {
     const feedback = await Feedback.find().sort({ _id: -1 })
     res.status(200).json(feedback)
   }
-  displayFeedback();
+  displayFeedback().catch(next);
 })
 
-app.post('/feedback', (req, res) => {
+app.post('/feedback', (req, res, next) => {
   const addFeedback = async () => {
-    const newFeedback = new Feedback({ id: req.body.id, text: req.body.text, rating: req.body.rating })
-    await newFeedback.save()
-    console.log(`saved ${newFeedback}`)
-    res.status(200).json(newFeedback)
+    const feedback = new Feedback({ id: req.body.id, text: req.body.text, rating: req.body.rating })
+    await feedback.save()
+    console.log(`saved item id ${feedback.id}`)
+    res.status(200).json(feedback)
   };
-  addFeedback();
+  addFeedback().catch(next);
 })
 
-app.delete('/feedback/:id', (req, res) => {
+app.delete('/feedback/:id', (req, res, next) => {
   const deleteFeedback = async () => {
     const feedback = await Feedback.deleteOne({ id: req.params.id })
-    console.log(`removed ${feedback}`)
+    console.log(`successfully deleted id ${req.params.id}`)
     res.status(200).json(feedback)
   }
-  deleteFeedback();
+  deleteFeedback().catch(next);
 })
 
-app.put('/feedback/:id', (req, res) => {
+app.put('/feedback/:id', (req, res, next) => {
   const updateFeedback = async () => {
     const feedback = await Feedback.findOne({ id: req.params.id })
     feedback.overwrite({ id: req.params.id, text: req.body.text, rating: req.body.rating });
     await feedback.save();
-    console.log(`updated ${feedback}`)
+    console.log(`updated item id ${feedback.id}`)
     res.status(200).json(feedback)
   }
-  updateFeedback();
+  updateFeedback().catch(next);
 })
+
+app.use((error, req, res, next) => {
+  console.log(error.toString())
+  return res.status(500).json({ error: error.toString() });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
